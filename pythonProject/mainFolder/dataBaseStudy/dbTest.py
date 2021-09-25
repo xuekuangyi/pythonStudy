b88fa610b082ce33b2f9701aec5129cc62e30ebb
xuekuangyi <xuekuangyi@gmail.com>
2021-09-25 22:44:42 +0800

    Initial commit

--- pythonProject/mainFolder/dataBaseStudy/dbTest.py
+++ pythonProject/mainFolder/dataBaseStudy/dbTest.py
@@ -6,19 +6,21 @@ import pymysql#pymysql是包名
 
 config = dict(host='47.94.251.62', user='root', password='root', database='standingBook')  # 构造函数
 # 建立数据库连接
-db = pymysql.connect(host=config['host'], user=config['user'], password=config['password'], database=config['database'])
+dbConnect = pymysql.connect(host=config['host'], user=config['user'], password=config['password'], database=config['database'])
 
 
 try:
-    cursor = db.cursor()
+    cursor = dbConnect.cursor()
     # sql
-    sql_so = "SELECT * FROM lr_base_invoice order by id"
+    sql_so = "SELECT * FROM lr_base_invoice order by id limit 10"
     cursor.execute(sql_so)
     result = cursor.fetchall()
     for record in result:
-        print(record)
+        dict = record
+        print( dict )
 except Exception:print("查询失败")
 
 # 关闭数据库连接
-db.close()
+print()
+dbConnect.close()
 print('finish')
