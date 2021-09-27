0a6ddc0a2866cfda942fdb3f87d7e737543639a2
xuekuangyi <xuekuangyi@gmail.com>
2021-09-28 03:00:19 +0800

    Initial commit

--- pythonProject/mainFolder/functionStudy/heart.py
+++ pythonProject/mainFolder/functionStudy/heart.py
@@ -44,8 +44,8 @@ def add11(*numbers):
     print(f'累加总和变量sum的格式及值:{type(sum), sum}')
     return sum
 
-add11(eval(input()))
 
+add11(eval(input()))
 
 
 def add2(*numbers):
@@ -59,11 +59,10 @@ def add2(*numbers):
     print(sum)
     return sum
 
+
 # add2(tuple(input()))
 # <class 'tuple'> (('1', ',', '2', ',', '3', ',', '4'),)
 # <class 'tuple'> ('1', ',', '2', ',', '3', ',', '4')
-
-
 
 
 def add3(*numbers):
@@ -94,17 +93,9 @@ def add4(numbers):
 # add2(input().split(','))
 
 
-
-
-
-
-
-
-
-
-
-
 # var1 = '3'
 # var2 = 4
 # print(f'var1的类型为:{type(var1)},var2的类型为{type(var2)}')
 # print(int(var1) + var2)
+
+# 测试更新2:59
